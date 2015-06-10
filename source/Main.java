58039f0c2821fe8a29a32a897ac4d7bfff6d0cdf
Alexander078 <alexguevara078@gmail.com>
2015-06-10 14:16:45 -0600

    practica git

---
+++ source/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexander
+ */
+import javax.swing.JOptionPane;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Esto es GIT");
+        
+    }
+    
+}
